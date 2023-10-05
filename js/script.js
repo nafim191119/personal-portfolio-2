24e4d1f2f9d56f01df977a3a17f69fcad8c43cd9
unknown <nafim191119@gmai.com>
2023-10-05 21:49:25 +0600

    portfolio-completed

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// Typing Animation //
+var typed = new Typed(".typing",{
+    strings:["MERN Stack Developer.", "Full Stack Developer.", "Front-End Developer."],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
